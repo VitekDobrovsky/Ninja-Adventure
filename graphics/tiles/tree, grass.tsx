1ab2c4d55b9da2b312c3f92e005d5bf72179fef1
Vitek Dobrovsky <vitek.dobrovsky@gmail.com>
2022-10-10 13:48:40 +0200

    1 level graphics and csv files

---
+++ graphics/tiles/tree, grass.tsx
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="tree, grass" tilewidth="128" tileheight="128" tilecount="8" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="128" height="128" source="trees/0.png"/>
+ </tile>
+ <tile id="1">
+  <image width="128" height="128" source="trees/01.png"/>
+ </tile>
+ <tile id="2">
+  <image width="128" height="128" source="trees/02.png"/>
+ </tile>
+ <tile id="3">
+  <image width="128" height="128" source="trees/03.png"/>
+ </tile>
+ <tile id="4">
+  <image width="128" height="128" source="trees/04.png"/>
+ </tile>
+ <tile id="5">
+  <image width="64" height="64" source="grass/grass_1.png"/>
+ </tile>
+ <tile id="6">
+  <image width="64" height="64" source="grass/grass_2.png"/>
+ </tile>
+ <tile id="7">
+  <image width="64" height="64" source="grass/grass_3.png"/>
+ </tile>
+</tileset>
--- support.py
+++ support.py
@@ -1,5 +1,6 @@
 import pygame
 from os import walk
+from csv import reader
 
 
 def  import_folder(path):
@@ -10,6 +11,15 @@ def  import_folder(path):
 			image_surface = pygame.image.load(full_path).convert_alpha()
 			img_list.append(image_surface)
 	return img_list
+
+def import_csv_layout(path):
+	terrain_map = []
+	with open(path) as level_map:
+		layout = reader(level_map,delimiter = ',')
+		for row in layout:
+			terrain_map.append(list(row))
+		return terrain_map
+
 
 def draw_rect(screen,x,y,width,height,color):
 	rect = pygame.Rect(x,y,width,height)
