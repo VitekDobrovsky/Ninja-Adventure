953ff6317ba40f98d5bead69d462d691dd906cd7
Vitek Dobrovsky <vitek.dobrovsky@gmail.com>
2022-10-13 18:19:31 +0200

    fix stump spawn bug

--- tile.py
+++ tile.py
@@ -11,12 +11,13 @@ class Tile(pygame.sprite.Sprite):
 		self.image = pygame.image.load(path).convert_alpha()
 		if type == 'grass':
 			self.image = choice(import_folder('graphics/tiles/grass')).convert_alpha()
-		elif type == 'tree':
-			self.image = choice(import_folder('graphics/tiles/trees')).convert_alpha()
 		elif type == 'stubbe':
 			self.image = choice(import_folder('graphics/tiles/logs')).convert_alpha()
+		elif type == 'tree':
+			self.image = choice(import_folder('graphics/tiles/trees')).convert_alpha()
 			
 		self.rect = self.image.get_rect(topleft=pos)
-		if type == 'tree':
+		if type == 'tree' or type == 'stubbe':
 			self.rect = self.image.get_rect(midleft=pos)
+
 		self.hitbox = self.rect.inflate(HITBOX_OFFSET_X[type], HITBOX_OFFSET_Y[type])
