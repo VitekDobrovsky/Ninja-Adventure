5b1815458d0159b3186c6d2b19c5c716e0b45840
Vitek Dobrovsky <vitek.dobrovsky@gmail.com>
2022-10-14 13:53:00 +0200

    added sprite sheet

--- graphics/tiles/TilesetHouse.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="TilesetHouse" tilewidth="32" tileheight="32" tilecount="3036" columns="66">
- <image source="TilesetHouse.png" width="2112" height="1472"/>
-</tileset>
