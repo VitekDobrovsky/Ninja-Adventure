f1faf8be04210d72a09bea1e5dd60b23d2e1c674
Vitek Dobrovsky <vit.dobrovsky@seznam.cz>
2023-05-14 16:57:01 +0200

    added main menu

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import pygame
 from sys import exit
 from settings import *
 from level import Level
-
+from support import Sprite_sheet
+from math import sin
 
 class NinjaAdventure:
 	def __init__(self):
@@ -14,7 +15,42 @@ class NinjaAdventure:
 		self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
 		self.clock = pygame.time.Clock()  
 		self.level = Level()
-  
+
+		# main menu
+		self.in_main_menu = True
+		self.heading_sheet = Sprite_sheet('graphics/GUI/NinjaAdventure2.png', (117, 25), ((72 * 10, 25 * 6)))
+		self.heading_frames = []
+		index = 0
+		for i in range(4):
+			self.heading_frames.append(self.heading_sheet.get_image(index,0))
+			index += 1
+
+		self.heading_index = 0
+		self.heding_speed = 0.05
+
+		self.heading = self.heading_frames[0]
+		self.heading_rect =self.heading.get_rect(center=(WIDTH / 2, HEIGHT / 7))
+		self.heading.set_alpha(0)
+
+		# sin wave motion
+		self.r = 10
+		self.theta = 0
+
+		self.font = pygame.font.Font('graphics/fonts/Gameplay.ttf' ,40)
+		self.play_txt = self.font.render('press SPACE to play!',True, (225,225,225))
+		self.play_rect = self.play_txt.get_rect(center=(WIDTH / 2, HEIGHT /2))
+
+	def main_menu(self):
+		if int(self.heading_index) <= 2:
+			self.heading.set_alpha(255)
+			self.heading = self.heading_frames[int(self.heading_index)]
+			self.heading_index += self.heding_speed
+		
+		self.heading_rect.y = (HEIGHT / 6) + self.r * sin(0.1 * self.theta)
+		self.theta += 0.3
+		self.screen.blit(self.heading, self.heading_rect)
+		self.screen.blit(self.play_txt, self.play_rect)
+
 	def run(self):
 		# main loop
 		while True:
@@ -25,15 +61,22 @@ class NinjaAdventure:
 					exit()
 					break
 			  
+			if pygame.key.get_pressed()[pygame.K_SPACE]:
+				if self.in_main_menu:
+					self.in_main_menu = False
 			# updating
-			self.screen.fill('#71DDEE')
-				
-				# restart game
-			if not self.level.player.restart:
-				self.level.run()
+			self.screen.fill('#4ca5b5')
+			if self.in_main_menu:
+				self.main_menu()
 			else:
-				self.level = Level()
-					
+				# restart game
+				if not self.level.player.restart:
+					self.level.run()
+				else:
+					self.level = Level()
+
+			
+
 			pygame.display.update()
 			self.clock.tick(FPS)
 
@@ -49,7 +92,6 @@ TODO:
 		enemy attack
 		kill bush
 		death
-		reset
 	
 	GRAPHICS:
 		dead sprites
@@ -62,6 +104,8 @@ TODO:
 
 	OTHER:
 		better gameplay
+		pause menu
 		main menu
 		get rid of other weapon
+		chose diferent font to play again text and center it
 '''
